0b4cdd338fa98d7aa215db3bec496e92cb5bbea4
Zoltan Kovago <zoltan.kovago@hu.ibm.com>
2023-09-10 17:26:03 +0200

    doc: Add some comments to the Vga_Timing module

--- src/Vga_Timing.v
+++ src/Vga_Timing.v
@@ -5,22 +5,30 @@
 `ifndef VGA_TIMING_V
 `define VGA_TIMING_V
 
+// Total width of the screen is 800 pixels.
 `define H_VISIBLE_AREA 640
 `define H_FRONT_PORCH   16
 `define H_PULSE         96
 `define H_BACK_PORCH    48
 
+// First clock cycle of the HSync pulse.
 `define H_PULSE_HEAD `H_VISIBLE_AREA + `H_FRONT_PORCH + 1
+// Last clock cycle of the HSync pulse.
 `define H_PULSE_TAIL `H_PULSE_HEAD   + `H_PULSE - 1
+// Last clock cycle of the horizontal counter.
 `define H_MAX        `H_PULSE_TAIL   + `H_BACK_PORCH
 
+// Total width of the screen is 600 pixels.
 `define V_VISIBLE_AREA 480
 `define V_FRONT_PORCH   10
 `define V_PULSE          2
 `define V_BACK_PORCH    33
 
+// First clock cycle of the VSync pulse.
 `define V_PULSE_HEAD `V_VISIBLE_AREA + `V_FRONT_PORCH + 1
+// Last clock cycle of the VSync pulse.
 `define V_PULSE_TAIL `V_PULSE_HEAD   + `V_PULSE - 1
+// Last clock cycle of the vertical counter.
 `define V_MAX        `V_PULSE_TAIL   + `V_BACK_PORCH
 
 `endif
